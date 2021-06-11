30f457188b9d0b2ac95dfc0405315bb624a2e811
Jeft <67933106+Jeftaei@users.noreply.github.com>
2021-06-11 11:19:27 -0500

    VEry Epic Chanegs that do Not matter at all

--- src/cogs/main.py
+++ src/cogs/main.py
@@ -21,12 +21,21 @@ class main(commands.Cog):
         self.alts = _altlist["alts"]
         self.send_to_channel = 586631641619759194
 
-        self.CountLaughablesKicks.start()
+        self.DumpLaughablesKicks.start()
 
         print("main Cog Loaded")
 
+    def reloadalts(self):
+        with open("alts.json", "r") as ajson3:
+            _altlist = json.load(ajson3)
+        self.alts = _altlist["alts"]
+
+    def dumpalts(self):
+        with open("alts.json", "w") as ajson4:
+            json.dump(_altlist, ajson4, indent=4)
+
     @tasks.loop(minutes=1)
-    async def CountLaughablesKicks(self):
+    async def DumpLaughablesKicks(self):
         with open("kicks.json", "w") as kjson2:
             json.dump( { "kicks" : self.kicks }, kjson2, indent=4 )
 
@@ -47,13 +56,9 @@ class main(commands.Cog):
         if ctx.author.id in losers:
             if member.id not in self.alts:
                 _altlist["alts"].append(member.id)
-                with open("alts.json", "w") as ajson:
-                    json.dump(_altlist, ajson, indent=4)
+                self.dumpalts()
+                self.reloadalts()
                 
-                # idk if i can read the files edited data right after i dumped shit into it
-                with open("alts.json", "r") as ajson2:
-                    self.alts = json.load(ajson2)["alts"]
-
                 await member.kick(reason="new laughable alt Poggers :trollxd:")
                 self.kicks += 1
 
